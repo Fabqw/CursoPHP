54057f49743b83a56144b537ab601ccbbc7cde51
Fabqw <qferfab27@gmail.com>
2024-05-17 13:07:48 -0400

    index

---
+++ Tarea2/index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de inicio</title>
+</head>
+<body>    
+    <nav>
+        <a href="#"><h3>Login</h3></a>
+        <a href="#"><h3>Registro de usuario</h3></a>
+    </nav>
+    <h3>Login</h3> <br>
+    <h3>Usuario</h3>    
+    <input type="text" class="form-control" name="producto" id="producto" placeholder="Nombre del producto:" value="">  
+    <h3>Contraseña</h3>
+    <input type="password" class="form-control" name="producto" id="producto" placeholder="Nombre del producto:" value="">  
+    
+</body>
+</html>
